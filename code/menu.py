d88eb02cb6fc367e924b5e1b62c30cc6670b3b08
Frederik Mogensen <frode.moge@gmail.com>
2024-03-18 12:45:25 +0100

    manual test of create

--- code/menu.py
+++ code/menu.py
@@ -17,5 +17,26 @@ session = db_connect.get_session()
 
 # import db_classes after the engine is created
 from db_classes import *
-
+# delete all tables
+Base.metadata.drop_all(db_connect.get_engine())
+# create tables
 Base.metadata.create_all(db_connect.get_engine())
+
+db_h = DatabaseHandler(session)
+user_h = UserHandler(session)
+admin_user_h = AdminUserHandler(session)
+
+# create admin user
+admin = admin_user_h.create_admin_user(
+                    username='admin',
+                    password='password',
+                    email='admin@email.com',
+                    admin_status='full'
+                    )
+
+# create user
+user = user_h.create_user(
+                    username='user',
+                    password='password',
+                    email='user@email.com',
+                    )
