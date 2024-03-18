86b02112d37d906cc04f3aa5ec77e6e29fb217a5
Frederik Mogensen <frode.moge@gmail.com>
2024-03-18 10:29:34 +0100

    forgot .py extension

---
+++ code/db_classes.py
@@ -0,0 +1,139 @@
+import uuid
+from sqlalchemy import Column, Integer, String, Float, Boolean, ForeignKey, DateTime
+from sqlalchemy.orm import declarative_base, relationship, validates
+from werkzeug.security import generate_password_hash, check_password_hash
+from datetime import datetime
+import re
+import json
+
+Base = declarative_base()
+
+class Product(Base):
+    __tablename__ = 'products'
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    uuid = Column(String, default=str(uuid.uuid4()), unique=True, nullable=False)
+    name = Column(String)
+    description = Column(String)
+    category_id = Column(Integer, ForeignKey('categories.id'))
+    price = Column(Float)
+    currency = Column(String)
+    quantity = Column(Integer)
+    
+    category = relationship('Category', back_populates='products')
+
+    @validates('price')
+    def validate_price(self, key, price):
+        if price < 0:
+            raise ValueError("Price must be positive")
+        return price
+    
+    @validates('quantity')
+    def validate_quantity(self, key, quantity):
+        if quantity <= 0:
+            raise ValueError("Quantity must be 0 or more")
+        return quantity
+
+class Category(Base):
+    __tablename__ = 'categories'
+    
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    name = Column(String, unique=True, nullable=False)
+    description = Column(String)
+    parent_id = Column(Integer, ForeignKey('categories.id'))
+    
+    parent = relationship('Category', remote_side=[id], backref='subcategories')
+    products = relationship('Product', back_populates='category')
+
+class Transaction(Base):
+    __tablename__ = 'transactions'
+    
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    uuid = Column(String, default=str(uuid.uuid4()), unique=True, nullable=False)
+    product_id = Column(Integer, ForeignKey('products.id'), nullable=False)
+    user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
+    date = Column(DateTime, default=datetime.now(), nullable=False)
+    profit = Column(Float, nullable=False)
+    currency = Column(String, nullable=False)
+    quantity = Column(Integer, nullable=False)
+    transaction_type = Column(String, nullable=False)
+
+    product = relationship('Product')
+    user = relationship('User')
+
+    @validates('transaction_type')
+    def validate_transaction_type(self, key, transaction_type):
+        allowed_types = ['purchase', 'refund', 'restock']
+        if transaction_type not in allowed_types:
+            raise ValueError(f"Invalid transaction type '{transaction_type}', allowed types are {allowed_types}")
+        return transaction_type
+    
+    @validates('quantity')
+    def validate_quantity(self, key, quantity):
+        if quantity < 0:
+            raise ValueError("Quantity must be positive")
+        return quantity
+    
+    @validates('currency')
+    def validate_currency(self, key, currency):
+        with open('../data/currencies.json', 'r') as f:
+            data = json.load(f)
+        allowed_currencies = data['allowed_currencies']
+        if currency not in allowed_currencies:
+            raise ValueError(f"Invalid currency '{currency}', allowed currencies are {allowed_currencies}")
+        return currency
+
+class User(Base):
+    __tablename__ = 'users'
+    
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    uuid = Column(String, default=lambda: str(uuid.uuid4()), unique=True)
+    username = Column(String(50), unique=True, nullable=False)
+    password = Column(String(100), nullable=False)
+    email = Column(String(100), unique=True, nullable=False)
+
+    __mapper_args__ = {
+        'polymorphic_identity':'user',
+        'polymorphic_on':type
+    }
+
+    def set_password(self, password):
+        self.password = generate_password_hash(password)
+    
+    def set_username(self, username):
+        self.username = username.lower()
+
+    def set_email(self, email):
+        self.email = email.lower()
+    
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
+
+    @validates('username')
+    def validate_username(self, key, username):
+        if len(username) < 3:
+            raise ValueError("Username must be at least 3 characters long")
+        return username.lower()
+
+    @validates('email')
+    def validate_email(self, key, email):
+        pattern = r"[^@]+@[^@]+\.[^@]+"
+        if not re.match(pattern, email):
+            raise ValueError("Invalid email")
+        return email
+
+class AdminUser(User):
+    __tablename__ = 'admin_users'
+    
+    id = Column(Integer, ForeignKey('users.id'), primary_key=True)
+    admin_status = Column(String(50), default='regular') # super, regular, etc.
+
+    __mapper_args__ = {
+        'polymorphic_identity':'admin_user',
+    }
+
+    @validates('admin_status')
+    def validate_admin_status(self, key, status):
+        allowed_statuses = ['regular', 'full']
+        if status not in allowed_statuses:
+            raise ValueError(f"Invalid admin status '{status}', allowed statuses are {allowed_statuses}")
+        return status
