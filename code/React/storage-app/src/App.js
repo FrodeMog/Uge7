dbdb4bcef5e6e50b2e90a637fa12ecc79aee37e5
Frederik Mogensen <frode.moge@gmail.com>
2024-03-25 15:50:38 +0100

    animation example

--- code/React/storage-app/src/App.js
+++ code/React/storage-app/src/App.js
@@ -1,13 +1,19 @@
 import React, {useState, useEffect} from 'react';
 import api from './Api';
+import './App.css';
 
 const App = () => {
   const [Users, setUsers] = useState([]);
+  const [selectedUser, setSelectedUser] = useState(null);
 
   const fetchUsers = async () => {
     const response = await api.get('/get_users/');
     setUsers(response.data);
   }
+
+  const handleUserClick = (user) => {
+    setSelectedUser(selectedUser === user ? null : user);
+  };
 
   useEffect(() => {
     fetchUsers();
@@ -23,23 +29,26 @@ const App = () => {
         </div>
       </nav>
       <h1 className="text-center">Users</h1>
-    <ul className="list-unstyled">
-        {Users.map((user) => (
-            <li key={user.id} className="mb-3">
-                <div className="card shadow p-3 m-3">
-                    <div className="card-body">
-                        <h5 className="card-title">{user.username}</h5>
-                        <p className="card-text">{user.email}</p>
-                        <p className="card-text">ID: {user.id}</p>
-                    </div>
-                </div>
-            </li>
-        ))}
-    </ul>
+      <ul className="list-unstyled">
+      {Users.map((user) => (
+        <li key={user.id} className="mb-3">
+          <div className="card shadow p-3 m-3" onClick={() => handleUserClick(user)}>
+            <div className={`card-body ${selectedUser === user ? 'open' : ''}`}>
+              <h5 className="card-title">{user.username}</h5>
+              {selectedUser === user && (
+                <>
+                  <p className="card-text">{user.email}</p>
+                  <p className="card-text">ID: {user.id}</p>
+                  <p className="card-text">Type: {user.type}</p>
+                </>
+              )}
+            </div>
+          </div>
+        </li>
+      ))}
+      </ul>
     </div>
   );
-
-
 }
 
 export default App;
